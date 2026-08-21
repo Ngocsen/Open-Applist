e9344ce1febf59e620ada23e70fbcba9a1a1287c
Ngọc Sen <70382284+Ngocsen@users.noreply.github.com>
2026-08-21 19:53:08 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,21 @@
 // sw.js
 self.addEventListener('install', (e) => {
-  e.waitUntil(self.skipWaiting());
+  e.waitUntil(
+    caches.open('snowboard-cache').then((cache) => {
+      return cache.addAll(['./', './index.html', './manifest.json']);
+    })
+  );
+  self.skipWaiting();
 });
+
 self.addEventListener('activate', (e) => {
   e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
