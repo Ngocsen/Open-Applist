d43e7ee2995bc45b76a179864eab81606e358a60
Ngọc Sen <70382284+Ngocsen@users.noreply.github.com>
2026-08-22 09:29:48 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,59 @@
-// sw.js
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('snowboard-cache').then((cache) => {
-      return cache.addAll(['./', './index.html', './manifest.json', './apple-touch-icon.png']);
+    caches.open('snowboard-v2').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './apple-touch-icon.png',
+        './db.json',
+        './social.json',
+        './system-app.json',
+        './Language/en_US.json',
+        './Language/vi_VN.json'
+      ]);
     })
   );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(self.clients.claim());
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== 'snowboard-v2').map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
+  // Dữ liệu JSON luôn ưu tiên mạng trước để luôn mới
+  if (e.request.url.includes('.json')) {
+    e.respondWith(
+      fetch(e.request)
+        .then((response) => {
+          const clone = response.clone();
+          caches.open('snowboard-v2').then((cache) => cache.put(e.request, clone));
+          return response;
+        })
+        .catch(() => caches.match(e.request))
+    );
+    return;
+  }
+
+  // Ảnh và static cache trước, mạng sau
   e.respondWith(
     caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+      return (
+        response ||
+        fetch(e.request).then((response) => {
+          const clone = response.clone();
+          caches.open('snowboard-v2').then((cache) => cache.put(e.request, clone));
+          return response;
+        })
+      );
     })
   );
 });
